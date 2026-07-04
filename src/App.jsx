570155c4e8b86016877a5c468fbc8bc347b049d3
itsrahulpal <itsmr.rahulpal@gmail.com>
2026-07-04 22:40:36 +0530

    Complete project & linked local folder to git Repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import TemperatureConvertor from "./components/TemperatureConverter";
-import Toggle from "./components/Toggle";
-import FormHandling from "./components/FormHandling";
+
 
 function App(){
   return(
     <>
-    {/* <Toggle/> */}
     <TemperatureConverter/>
-    {/* <FormHandling/> */}
     </>
   );
 };
